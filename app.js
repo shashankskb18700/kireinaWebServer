8086bdb8cd7fe1f076235e7bafebefbe90086b9d
shashank <shashankskb18700@gmail.com>
2022-07-13 00:44:04 +0530

    fixed cors error

--- app.js
+++ app.js
@@ -14,6 +14,18 @@ const app = express();
 
 app.use(express.json());
 
+const whitelist = ["https://kireinanime.web.app/"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
 app.get("/serve", (req, res) => {
   console.log("herer");
   res.set("Access-Control-Allow-Origin", "*");
