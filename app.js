6ad813839a13d9a96b4e58e9d3065f54ad1df897
shashank <shashankskb18700@gmail.com>
2022-07-16 00:36:24 +0530

    ad

--- app.js
+++ app.js
@@ -76,10 +76,10 @@ app.post("/search", async (req, res) => {
   //   console.log(`[ERROR]  ${err}`);
   // };
 
-  let j = await axios.get(
-    "https://neko-sama.fr/anime/info/7892-koe-no-katachi-vostfr"
-  );
-  console.log(j);
+  // let j = await axios.get(
+  //   "https://neko-sama.fr/anime/info/7892-koe-no-katachi-vostfr"
+  // );
+  // console.log(j);
 
   // var d;
 
