28fd21e05f39df62d613cc0e6139f4aef0424fbe
shashank <shashankskb18700@gmail.com>
2022-07-22 01:27:09 +0530

    da

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const corsOptions = {
 };
 
 // if you want to use it in offline means on local  host comment next line of code ;
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 // app.use(cors({ origin: "https://kireinanime.web.app/", credentials: true }));
 app.get("/", async (req, res) => {
